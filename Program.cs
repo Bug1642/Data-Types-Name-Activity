ac268823df1b44bb06dada4d00fa0db6af2219f0
Bug1642 <hasancan1642@hotmail.com>
2024-12-23 12:37:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+namespace DataType
+{
+  public class StudentInput
+  {
+    public string Name()
+    {
+        string name ="Hasan";
+        return name;
+    }
+  }
+  class Program {
+    static void Main (string[] args){
+
+        StudentInput student = new StudentInput();
+        Console.WriteLine($"Student Name is: {student.Name()}");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Name
+ Data-Types-Name-Activity
